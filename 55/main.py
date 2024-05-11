8a65c8c61cfb939dba5d19224bad9d2573f23e35
Parham Alvani <parham.alvani@gmail.com>
2024-05-11 19:36:49 +0000

    feat: add solution to a leet code problem

---
+++ 55/main.py
@@ -0,0 +1,26 @@
+def can_jump(nums: list[int]) -> bool:
+    queue: list[int] = []
+    seen: set[int] = set()
+
+    if len(nums) == 1:
+        return True
+
+    queue.append(0)
+    seen.add(0)
+
+    while len(queue) != 0:
+        index = queue.pop(0)
+
+        for i in range(index + 1, min(index + 1 + nums[index], len(nums))):
+            if i not in seen:
+                seen.add(i)
+                if i == len(nums) - 1:
+                    return True
+                queue.append(i)
+
+    return False
+
+
+if __name__ == "__main__":
+    assert can_jump([2, 3, 1, 1, 4]) == True
+    assert can_jump([3, 2, 1, 0, 4]) == False
