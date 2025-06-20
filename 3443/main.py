775625e2514b864833e4cc99926d7c9354f9faac
Parham Alvani <parham.alvani@gmail.com>
2025-06-20 21:45:53 +0000

    feat: add new question

---
+++ 3443/main.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxDistance(self, s: str, k: int) -> int:
+        ans = 0
+        north = south = east = west = 0
+
+        for i in range(len(s)):
+            c = s[i]
+            if c == "N":
+                north += 1
+            elif c == "S":
+                south += 1
+            elif c == "E":
+                east += 1
+            elif c == "W":
+                west += 1
+
+            x = abs(north - south)
+            y = abs(east - west)
+            MD = x + y
+            dis = MD + min(2 * k, i + 1 - MD)
+            ans = max(ans, dis)
+
+        return ans
+
+
+if __name__ == "__main__":
+    assert Solution().maxDistance("NWSE", 1) == 3
+    assert Solution().maxDistance("NSWWEW", 3) == 6
